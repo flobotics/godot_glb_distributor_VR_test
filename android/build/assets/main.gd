a08b163d1645cb8b755bd33940fc75bef4a45765
inflo <inflo@web.de>
2023-01-21 01:19:32 +0100

    first commit

---
+++ android/build/assets/main.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var interface : XRInterface
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		print("OpenXR initialised successfully")
+
+		#var vp : Viewport = get_viewport()
+		#vp.use_xr = true
+		
+		get_viewport().use_xr = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
